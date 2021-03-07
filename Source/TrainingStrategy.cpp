1976b1305b4c3b36c2a5b313be1fb18bd78d5f2e
AdotBdot <42146911+AdotBdot@users.noreply.github.com>
2021-03-07 13:18:45 +0100

    Added some definitions

--- Source/TrainingStrategy.cpp
+++ Source/TrainingStrategy.cpp
@@ -1,1 +1,39 @@
 #include "TrainingStrategy.h"
+
+#include <algorithm>
+
+using std::move;
+
+abnn::TrainingStrategy::TrainingStrategy()
+	:m_NeuralNet( nullptr )
+	, m_DataSet( nullptr )
+	, m_LearningRate( 0.7f )
+	, m_DesiredError( 0.1f )
+{
+}
+
+abnn::TrainingStrategy::TrainingStrategy( NeuralNet* NeuralNetPtr, DataSet* DataSerPtr )
+	:m_NeuralNet( NeuralNetPtr )
+	, m_DataSet( DataSerPtr )
+	, m_LearningRate( 0.7f )
+	, m_DesiredError( 0.1f )
+{
+}
+
+abnn::TrainingStrategy::~TrainingStrategy()
+{
+}
+
+void abnn::TrainingStrategy::setLearningRate( float Rate )
+{
+	m_LearningRate = move( Rate );
+}
+
+void abnn::TrainingStrategy::setDesiredError( float Goal )
+{
+	m_DesiredError = move( Goal );
+}
+
+void abnn::TrainingStrategy::print()
+{
+}
--- Source/TrainingStrategy.h
+++ Source/TrainingStrategy.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "NeuralNet.h"
+#include "DataSet.h"
+
 namespace abnn
 {
 	class TrainingStrategy
@@ -10,7 +13,20 @@ namespace abnn
 			MSE
 		};
 
+		TrainingStrategy();
+		TrainingStrategy( NeuralNet* NeuralNetPtr, DataSet* DataSerPtr );
+		~TrainingStrategy();
+
+		void setLearningRate( float Rate );
+		void setDesiredError( float Goal );
+
+		void print();
+
 	private:
+		NeuralNet* m_NeuralNet;
+		DataSet* m_DataSet;
+
 		float m_LearningRate;
+		float m_DesiredError;
 	};
 }
